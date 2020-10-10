400babf181c6fb2882cac8c3f5de03f8cf980a83
haoyu <kevin349831@gmail.com>
2020-10-11 00:16:30 +0800

    audio test

--- includes/mp3.php
+++ includes/mp3.php
@@ -2,7 +2,26 @@
 
 if (strtolower($message['text']) == "audio" || $message['text'] == "音頻" || $message['text'] == "音樂") {
 
-    $audiofileurl = 'https://kasavakan-linebot.herokuapp.com/assets/audios/A.mp3'; // 音樂文件網址
+		$file = fopen('../assets/audios/music.mp3', 'wb');
+
+		$cacheFileName = '../assets/audios/A.mp3';
+		$cacheFile     = fopen($cacheFileName, 'rb');
+		$content       = fread($cacheFile, filesize($cacheFileName));
+		fwrite($file, $content);
+		fclose($cacheFile);
+
+		$cacheFileName = '../assets/audios/B.mp3';
+		$cacheFile     = fopen($cacheFileName, 'rb');
+		$content       = fread($cacheFile, filesize($cacheFileName));
+		fwrite($file, $content);
+		fclose($cacheFile);
+
+		fclose($file);
+
+
+
+
+    $audiofileurl = 'https://kasavakan-linebot.herokuapp.com/assets/audios/music.mp3'; // 音樂文件網址
 
     $milliseconds = '1000'; // 音樂長度 (毫秒)
 
