e3f5e40b0559ebb8dacebdcf2e4209b58848da33
haoyu <kevin349831@gmail.com>
2020-10-10 22:37:49 +0800

    audio test

--- includes/temp/audio.php
+++ includes/temp/audio.php
@@ -4,7 +4,7 @@ if (strtolower($message['text']) == "audio" || $message['text'] == "音頻" || $
 
     $audiofileurl = 'https://kasavakan-linebot.herokuapp.com/assets/audios/a.ogg'; // 音樂文件網址
 
-    $milliseconds = '3000'; // 音樂長度 (毫秒)
+    $milliseconds = '1000'; // 音樂長度 (毫秒)
 
     $client->replyMessage(array(
         'replyToken' => $event['replyToken'],
