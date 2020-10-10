47252bd8b7630e4804f2b4c12d6ba173b19d799e
haoyu <kevin349831@gmail.com>
2020-10-10 22:46:08 +0800

    audio test

--- includes/mp3.php
+++ includes/mp3.php
@@ -2,7 +2,7 @@
 
 if (strtolower($message['text']) == "audio" || $message['text'] == "音頻" || $message['text'] == "音樂") {
 
-    $audiofileurl = 'https://kasavakan-linebot.herokuapp.com/assets/audios/a.ogg'; // 音樂文件網址
+    $audiofileurl = 'https://api.reh.tw/line/bot/example/assets/audios/example.ogg'; // 音樂文件網址
 
     $milliseconds = '1000'; // 音樂長度 (毫秒)
 
