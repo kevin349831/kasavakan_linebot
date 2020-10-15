52e3089937d42f481a78dac6dc0344a9b92e4643
haoyu <kevin349831@gmail.com>
2020-10-15 20:54:31 +0800

    audio

--- includes/mp3.php
+++ includes/mp3.php
@@ -2,9 +2,9 @@
 
 if (strtolower($message['text']) == "audio" || $message['text'] == "音頻" || $message['text'] == "音樂") {
 		// 用指令合併mp3檔案 可以合併多個
-		$A = exec('cat ./assets/audios/A.mp3 ./assets/audios/A.mp3 > ./assets/audios/new.mp3', $output, $return_val);
+		$A = exec('cat ./assets/audios/ka.mp3 ./assets/audios/sa.mp3 ./assets/audios/va.mp3 ./assets/audios/kan.mp3 > ./assets/audios/new.mp3', $output, $return_val);
 		//播放檔案位置 需要永久固定 因為會直接抓那個檔案來播放
-    $audiofileurl = 'https://kasavakan-linebot.herokuapp.com/assets/audios/sa.mp3'; // 音樂文件網址
+    $audiofileurl = 'https://kasavakan-linebot.herokuapp.com/assets/audios/new.mp3'; // 音樂文件網址
 
     $milliseconds = '3000'; // 音樂長度 (毫秒)
 
