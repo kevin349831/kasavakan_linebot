5a279a36c5988f11a08852d4e2ab343f590df0d6
haoyu <kevin349831@gmail.com>
2021-01-26 16:16:49 +0800

    new data

--- includes/database.php
+++ includes/database.php
@@ -3,13 +3,13 @@
 
 if (strtolower($message['text']) == "DB"){
 
-    //DB
+    //------------DB
     $host = '210.240.162.61';
     //改成你登入phpmyadmin帳號
     $user = 'savak999';
     //改成你登入phpmyadmin密碼
     $passwd = 'savak999';
-
+    //資料庫名稱
     $database = 'savak999_DB';
     //實例化mysqli(資料庫路徑, 登入帳號, 登入密碼, 資料庫)
     $connect = new mysqli($host, $user, $passwd, $database);
@@ -17,6 +17,35 @@ if (strtolower($message['text']) == "DB"){
     if ($connect->connect_error) {
         die("連線失敗: " . $connect->connect_error);
     }
+    echo "連線成功";
+
+    //設定連線編碼，防止中文字亂碼
+    $connect->query("SET NAMES 'utf8'");
+
+    //選擇資料表user，條件是欄位id = 1的
+    $selectSql = "SELECT * FROM `kasavakan` WHERE `dialects_word` = 'sahar'";
+    //echo $selectSql;
+    //呼叫query方法(SQL語法)
+    $memberData = $connect->query($selectSql);
+    //有資料筆數大於0時才執行
+    if ($memberData->num_rows > 0) {
+    //讀取剛才取回的資料
+        while ($row = $memberData->fetch_assoc()) {
+            //print_r($row);
+            $newData = $row;
+        }
+    } else {
+        echo '0筆資料';
+    }
+
+    //print($newData['dialects_word']);
+    //print($newData['chinese_word']);
+    //print($newData['dialects_sentence']);
+    //print($newData['chinese_sentence']);
+    //print($newData['radical_word']);
+
+    $connect -> close();
+    //--------------DB
 
 
     $client->replyMessage(array(
@@ -24,7 +53,7 @@ if (strtolower($message['text']) == "DB"){
         'messages' => array(
             array(
                 'type' => 'text', // 訊息類型 (文字)
-                'text' => 'Hello, connect success.' // 回復訊息
+                'text' => $newData['dialects_word']; // 回復訊息
             )
         )
     ));
