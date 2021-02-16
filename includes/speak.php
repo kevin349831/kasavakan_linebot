4f2f8ab46a518a55c5ab86fed117fbe56fcc9d43
haoyu <kevin349831@gmail.com>
2021-02-16 15:22:09 +0800

    test

--- includes/speak.php
+++ includes/speak.php
@@ -2,7 +2,11 @@
 $NewString = strtolower($message['text']);
 $NewCheckWord = str_split($NewString,1); //.word
 if (strtolower($NewCheckWord[0]) == ".") { // 檢查是不是.開頭
-
+  //如果第一個字是. 就把它移除
+  $Checkdesh = str_split($NewString,1);
+  if($Checkdesh[0] == '.') {
+    $NewString = substr($NewString,1,strlen($NewString));
+  }
 // 族語拆字
 $word = strtolower($message['text']);
 $CheckWord = str_split($word,1);
