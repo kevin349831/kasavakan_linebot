c447ad2aa9d9e5a1d8dec647a9b9535beb09b7a2
haoyu <kevin349831@gmail.com>
2021-02-16 13:57:24 +0800

    update

--- includes/select.php
+++ includes/select.php
@@ -7,11 +7,11 @@ if ($message['text'] == "DB" || $message['text'] == "資料庫"){
     $con = "dbname=d74b535jl61vqu host=ec2-100-24-139-146.compute-1.amazonaws.com port=5432 user=erlewlqwfxroic password=24a136a16b48e1a0e38c616d42af528d8280cdbbb933393dde8fa83da2cee8b0 sslmode=require";
     if (!$con)
     {
-      echo "Database connection failed.";
+      $theword = "Database connection failed.";
     }
     else
     {
-      echo "Database connection success.";
+      $theword = "Database connection success.";
     }
     //--------------DB
 
