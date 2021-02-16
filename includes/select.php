0f3e6c31ba1e9743a680f534a6154ff88926bf11
haoyu <kevin349831@gmail.com>
2021-02-16 14:21:59 +0800

    test

--- includes/select.php
+++ includes/select.php
@@ -2,22 +2,18 @@
 
 
 if ($message['text'] == "DB" || $message['text'] == "資料庫"){
-    $theword = '';
+  $theword = '';
+  $host        = "host=ec2-100-24-139-146.compute-1.amazonaws.com";
+ $port        = "port=5432";
+ $dbname      = "dbname=d74b535jl61vqu";
+ $credentials = "user=erlewlqwfxroic password=24a136a16b48e1a0e38c616d42af528d8280cdbbb933393dde8fa83da2cee8b0";
 
-    $con = "dbname=d74b535jl61vqu host=ec2-100-24-139-146.compute-1.amazonaws.com port=5432 user=erlewlqwfxroic password=24a136a16b48e1a0e38c616d42af528d8280cdbbb933393dde8fa83da2cee8b0 sslmode=require";
-    if (!$con)
-    {
-      $theword = "Database connection failed.";
-    }
-    else
-    {
-      $theword = "Database connection success.";
-    }
-
-    $result = pg_query($con,"SELEC FROM kasavakan WHERE ethnic like 'suwan'");
-    while($row=pg_fetch_assoc($result)){
-      $theword = $theword.$row['ethnic'];
-    }
+ $db = pg_connect( "$host $port $dbname $credentials"  );
+ if(!$db){
+    $theword = "Error : Unable to open database";
+ } else {
+    $theword = "Opened database successfully";
+ }
     //--------------DB
 
 
