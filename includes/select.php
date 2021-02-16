cc92fc3d5f55d5e3f589f116d923295d7081695b
haoyu <kevin349831@gmail.com>
2021-02-16 13:50:51 +0800

    delete sqlite and add postgresql

--- includes/select.php
+++ includes/select.php
@@ -1,19 +1,21 @@
 <?php
 
 
-if ($message['text'] == "SQ" || $message['text'] == "sq"){
+if ($message['text'] == "DB" || $message['text'] == "資料庫"){
     $theword = '';
-    class SQLiteDB extends SQLite3{
-        function __construct(){
-           $this->open('savak.db');
-        }
-     }
-     $db = new SQLiteDB();
-     if(!$db){
-        $theword = $db->lastErrorMsg();
-     } else {
-        $theword = "Yes, Opened database successfully";
-     }
+    
+    $host        = "ec2-100-24-139-146.compute-1.amazonaws.com";
+    $port        = "port=5432";
+    $dbname      = "dbname=d74b535jl61vqu";
+    $credentials = "user=erlewlqwfxroic password=24a136a16b48e1a0e38c616d42af528d8280cdbbb933393dde8fa83da2cee8b0";
+
+    $db = pg_connect( "$host $port $dbname $credentials"  );
+    if(!$db){
+       $theword = "Error : Unable to open database";
+    } else {
+       $theword = "Opened database successfully";
+    }
+    //--------------DB
 
 
     $client->replyMessage(array(
@@ -21,7 +23,7 @@ if ($message['text'] == "SQ" || $message['text'] == "sq"){
         'messages' => array(
             array(
                 'type' => 'text', // 訊息類型 (文字)
-                'text' => 'SQ文字：'.$theword // 回復訊息
+                'text' => 'postgresql 文字：'.$theword // 回復訊息
             )
         )
     ));
