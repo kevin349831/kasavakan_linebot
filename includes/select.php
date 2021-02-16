85da237c57e305aa58c971d422b311e7db914c7d
haoyu <kevin349831@gmail.com>
2021-02-16 13:56:01 +0800

    update

--- includes/select.php
+++ includes/select.php
@@ -3,17 +3,15 @@
 
 if ($message['text'] == "DB" || $message['text'] == "資料庫"){
     $theword = '';
-    
-    $host        = "ec2-100-24-139-146.compute-1.amazonaws.com";
-    $port        = "port=5432";
-    $dbname      = "dbname=d74b535jl61vqu";
-    $credentials = "user=erlewlqwfxroic password=24a136a16b48e1a0e38c616d42af528d8280cdbbb933393dde8fa83da2cee8b0";
 
-    $db = pg_connect( "$host $port $dbname $credentials"  );
-    if(!$db){
-       $theword = "Error : Unable to open database";
-    } else {
-       $theword = "Opened database successfully";
+    $con = "dbname=d74b535jl61vqu host=ec2-100-24-139-146.compute-1.amazonaws.com port=5432 user=erlewlqwfxroic password=24a136a16b48e1a0e38c616d42af528d8280cdbbb933393dde8fa83da2cee8b0 sslmode=require";
+    if (!$con)
+    {
+      echo "Database connection failed.";
+    }
+    else
+    {
+      echo "Database connection success.";
     }
     //--------------DB
 
